2b76066ce5d08ddb1eabfef028c7846035434274
tornikegomareli <gomarelidevelopment@gmail.com>
2026-09-20 12:10:03 +0400

    findsfsymbols 0.1.2

--- Casks/findsfsymbols.rb
+++ Casks/findsfsymbols.rb
@@ -1,12 +1,14 @@
 cask "findsfsymbols" do
-  version "0.1.1"
-  sha256 "804624ca8640e2ea19142e83dc42cf18595d692e6a98c9e15ecdae27853bbbe5"
+  version "0.1.2"
+  sha256 "397c3e45462d53f86d660ffda9f304676c6d875a0aa1e7b83a25c0d65325bb4b"
 
   url "https://github.com/tornikegomareli/FindSFSymbols/releases/download/v#{version}/FindSFSymbols-#{version}.zip"
   name "FindSFSymbols"
   desc "Semantic SF Symbols search with a physics pile"
   homepage "https://tornikegomareli.github.io/FindSFSymbols/"
 
+  # The app updates itself with Sparkle. This stops brew from reporting it as outdated.
+  auto_updates true
   depends_on macos: :sonoma
 
   app "FindSFSymbols.app"
